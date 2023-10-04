b65cedb51e062c025c9a64f54c990119221f83ba
Mykhailo Pashchenkoe <bdemlolp234@gmail.com>
2023-10-04 10:29:03 +0300

    added task2

---
+++ main.py
@@ -0,0 +1,16 @@
+# def check(text: list):
+#     bb = set(text)
+#     print(bb)
+# check(input(".."))
+
+# task2
+
+# def get_elements(text: str):
+#     new_list = list(text)
+#     check = len(set(new_list))
+#     print(check)
+#
+#
+# get_elements(input(">>>"))
+
+#task3
